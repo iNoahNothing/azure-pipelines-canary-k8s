3f39992498324c11f4da91a63fd66a5ae80de5d3
Noah Krause <krausenoah@gmail.com>
2020-02-14 14:54:31 -0500

    canary build
    Signed-off-by: Noah Krause <krausenoah@gmail.com>

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ from prometheus_client import start_http_server, Counter
 app = Flask('sampleapp')
 c = Counter('requests', 'Number of requests served, by custom_status', ['custom_status'])
 
-success_rate = 10
+success_rate = 20
 
 @app.route('/')
 def hello():
